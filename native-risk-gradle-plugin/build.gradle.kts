276db84cc6970c0693ad6f76c6ba03da4555582f
riyashikanedunchezhian-lgtm <riyashikanedunchezhian@gmail.com>
2026-08-20 19:53:50 +0530

    Fix functional test plugin metadata wiring

--- native-risk-gradle-plugin/build.gradle.kts
+++ native-risk-gradle-plugin/build.gradle.kts
@@ -42,3 +42,6 @@ val functionalTest = tasks.register<Test>("functionalTest") {
 tasks.check {
     dependsOn(functionalTest)
 }
+tasks.named('functionalTest') {
+    dependsOn publishToMavenLocal
+}
